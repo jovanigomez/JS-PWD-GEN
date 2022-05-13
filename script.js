1f6d7b0e7387793105d7ebc5fedf327d55a9aef3
jovanigomez <jovani.gomez@gmail.com>
2022-05-12 22:49:47 -0700

    pushing

---
+++ script.js
@@ -0,0 +1,14 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+  var password = document.querySelector("#password");
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
