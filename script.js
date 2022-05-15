5a1abaaef46e333e07fe3a2af1e314f9d3cd8dff
jovanigomez <jovani.gomez@gmail.com>
2022-05-15 10:06:28 -0700

    trouble shooting merge issue

--- script.js
+++ script.js
@@ -2,13 +2,28 @@
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
+function generatePassword() {
+  var chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ!@$%^&*()abcdefjhijklmnopqrstuvwxyz";
+  var password = "";
+  var password = 8;
+ return 'password';
+}
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
   var password = document.querySelector("#password");
+
+
+
+  
+  
+  
+  
+
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
