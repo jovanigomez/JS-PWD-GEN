98a5022999213ee9a8df722b45bed847c6ddbbc6
jovanigomez <jovani.gomez@gmail.com>
2022-05-16 00:14:09 -0700

    adding length

--- script.js
+++ script.js
@@ -3,28 +3,38 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 var uppercaseChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
 var lowercaseChar = "abcdefghijklmnopqrstuvwxyz";
 
+var numericalChar = "01234567890";
+
+var specialChar = "!@#$%^&*()_+";
 
 function generatePassword() {
+ 
+  var password = "";
+  var passwordChar = "";
   
-}
-function writePassword() {
+  var passwordLenght = prompt("8 characters")
+  passwordLengthUser = parseInt(passwordLength);
+  
+  
+
+  
+
+  function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-  var password = document.querySelector("#password");
-
-
-
+  var password = document.querySelector("#password"); 
   
-  
-  
-  
+  }
 
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
 
+ generateBtn.addEventListener("click", writePassword);
+
+ 
