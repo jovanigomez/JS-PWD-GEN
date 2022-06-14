533a87de40f37874b9be8d9ff889642ddb2d9020
jovanigomez <jovani.gomez@gmail.com>
2022-06-13 19:45:37 -0700

    fixing vars

--- assets/script.js
+++ assets/script.js
@@ -2,15 +2,14 @@
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-var uppercaseChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-var lowercaseChar = "abcdefghijklmnopqrstuvwxyz";
-
-var numericalChar = "01234567890";
-
-var specialChar = "!@#$%^&*()_+";
-
 function generatePassword() {
+var uppercaseChar = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var lowercaseChar = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var numericalChar = ["0","1","2","3","4","5","6","7","8","9"];
+var specialChar = ["!","@","#","$","%","^","&","*","(",")","_","+"];
+  var uppercaseChar = lowercaseChar.map(element => {
+return element.toUpperCase();
+});
 
   var password = "";
   var passwordCharacters = "";
