16ac1a03d880fd23a2fb8f742f3dee0951121b23
jovanigomez <jovani.gomez@gmail.com>
2022-06-21 18:16:19 -0700

    fixing typo

--- assets/script.js
+++ assets/script.js
@@ -7,21 +7,20 @@ var uppercaseChar = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O"
 var lowercaseChar = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var numericalChar = ["0","1","2","3","4","5","6","7","8","9"];
 var specialChar = ["!","@","#","$","%","^","&","*","(",")","_","+"];
-  var uppercaseChar = lowercaseChar.map(element => {
+var uppercaseChar = lowercaseChar.map(element => {
 return element.toUpperCase();
 });
 
-  var passwordlength = prompt("how long would yuo like your pass word to be? 8 or 128");
+  var passwordlength = prompt("how long would you like your password to be 8 or 128?", "yes") == "yes";
   var includelowercasecharacter = prompt("would you like lowercase?", "yes") == "yes"; 
   console.log(includelowercasechar);
   var includeuppercasechar = prompt("want some upper case?", "yes") == "yes";
   var includespecialchar = prompt("wanna sprinkle some speacial characters?", "yes") == "yes";
-  var numericalChar = prompt("would you like some numbers too?","yes") == "yes";
+  var numericalchar = prompt("would you like some numbers too?","yes") == "yes";
 
   let password = "";
   
-  
-  
+ 
   
 
   
