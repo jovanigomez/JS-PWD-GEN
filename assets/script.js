77fc4a3083fe5efe2173b84b75a6b769ca758f96
jovanigomez <jovani.gomez@gmail.com>
2022-06-26 22:28:15 -0700

    debuggin

--- assets/script.js
+++ assets/script.js
@@ -3,15 +3,15 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function generatePassword() {
-var uppercaseChar = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-var lowercaseChar = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-var numericalChar = ["0","1","2","3","4","5","6","7","8","9"];
-var specialChar = ["!","@","#","$","%","^","&","*","(",")","_","+"];
-var uppercaseChar = lowercaseChar.map(element => {
+var uppercaseCharacters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var lowercaseCharacters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var numericalCharacters = ["0","1","2","3","4","5","6","7","8","9"];
+var specialCharacters = ["!","@","#","$","%","^","&","*","(",")","_","+"];
+var uppercaseCharacters = lowercaseCharacters.map(element => {
 return element.toUpperCase();
 });
 
-  var passwordlength = prompt("how long would you like your password to be 8 or 128?", "yes") == "yes";
+  var passwordlength = prompt("how long would you like your password to be 8 or 128?");
   var includelowercasecharacter = prompt("would you like lowercase?", "yes") == "yes"; 
   console.log(includelowercasecharacter);
   var includeuppercasecharacters = prompt("want some uppercase?", "yes") == "yes";
@@ -20,28 +20,32 @@ return element.toUpperCase();
 
   let password = "";
   
- console.log(uppercaseCharacters);
-  
+ //console.log(includeuppercasecharacters);
+ //console.log(includenumericalcharacters);
+ //console.log(includespeacialcharacters);
+ 
+
   while (password.length <= passwordlength) {
     if (includelowercasecharacter) {
-    if (password,length == passwordlength) break;
-   password += lowercaseCharacters[Math.floor(Math.random() * lowercaseChar.length)];
+    if (password.length == passwordlength) break;
+   password += lowercaseCharacters[Math.floor(Math.random() * lowercaseCharacters.length)];
     }
     if (includespeacialcharacters) {
       if (password.length == passwordlength) break;
-      password += specialCharacters[Math.floor(Math.random() * specialChar.length)];
+      password += specialCharacters[Math.floor(Math.random() * specialCharacters.length)];
     }
     if (includenumericalcharacters) {
       if (password.length == passwordlength) break;
-      password += numericalCharacters[Math.floor(Math.random() * numericalChar.length)];
+      password += numericalCharacters[Math.floor(Math.random() * numericalCharacters.length)];
     }
     if (includeuppercasecharacters) {
       if (password.length == passwordlength) break;
-      password += uppercaseCharacters[Math.floor(Math.random() * uppercaseChar.length)];
+      password += uppercaseCharacters[Math.floor(Math.random() * uppercaseCharacters.length)];
     }
   }
   
   const shuffled = password.split('').sort(function() {return 0.5-Math.random()}).join('');
+  
   return shuffled;
   
 }
